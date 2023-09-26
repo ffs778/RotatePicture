87eb945ed6051a3891dee9c4d6e6203f2afbb349
王海平 <2530611886@qq.com>
2023-09-26 17:30:39 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.create_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.originPictureAngle_tbx = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.finalPictureAngle_tbx = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deltaAngle_tbx = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.playRotatePics_btn = new System.Windows.Forms.Button();
+            this.playPics_picb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.playPics_picb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // create_btn
+            // 
+            this.create_btn.Location = new System.Drawing.Point(356, 288);
+            this.create_btn.Name = "create_btn";
+            this.create_btn.Size = new System.Drawing.Size(172, 77);
+            this.create_btn.TabIndex = 1;
+            this.create_btn.Text = "生成旋转后图片";
+            this.create_btn.UseVisualStyleBackColor = true;
+            this.create_btn.Click += new System.EventHandler(this.create_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "输入当前图片的旋转角度";
+            // 
+            // originPictureAngle_tbx
+            // 
+            this.originPictureAngle_tbx.Location = new System.Drawing.Point(428, 24);
+            this.originPictureAngle_tbx.Name = "originPictureAngle_tbx";
+            this.originPictureAngle_tbx.Size = new System.Drawing.Size(86, 23);
+            this.originPictureAngle_tbx.TabIndex = 6;
+            this.originPictureAngle_tbx.TextChanged += new System.EventHandler(this.currentPictureAngle_tbx_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(543, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 27);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "图片旋转";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(534, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.finalPictureAngle_tbx);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.deltaAngle_tbx);
+            this.groupBox1.Controls.Add(this.create_btn);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.originPictureAngle_tbx);
+            this.groupBox1.Location = new System.Drawing.Point(45, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1177, 445);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "图片生成";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "最后一张图片的旋转角度";
+            // 
+            // finalPictureAngle_tbx
+            // 
+            this.finalPictureAngle_tbx.Location = new System.Drawing.Point(428, 130);
+            this.finalPictureAngle_tbx.Name = "finalPictureAngle_tbx";
+            this.finalPictureAngle_tbx.Size = new System.Drawing.Size(86, 23);
+            this.finalPictureAngle_tbx.TabIndex = 16;
+            this.finalPictureAngle_tbx.TextChanged += new System.EventHandler(this.lastPictureAngle_tbx_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "每次变化的°数";
+            // 
+            // deltaAngle_tbx
+            // 
+            this.deltaAngle_tbx.Location = new System.Drawing.Point(428, 78);
+            this.deltaAngle_tbx.Name = "deltaAngle_tbx";
+            this.deltaAngle_tbx.Size = new System.Drawing.Size(86, 23);
+            this.deltaAngle_tbx.TabIndex = 14;
+            this.deltaAngle_tbx.TextChanged += new System.EventHandler(this.deltaAngle_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox2.Controls.Add(this.playRotatePics_btn);
+            this.groupBox2.Controls.Add(this.playPics_picb);
+            this.groupBox2.Location = new System.Drawing.Point(45, 528);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1177, 473);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "旋转预览";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(999, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "打开文件位置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OpenFileLocation_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(54, 22);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 400);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
+            // playRotatePics_btn
+            // 
+            this.playRotatePics_btn.Location = new System.Drawing.Point(453, 173);
+            this.playRotatePics_btn.Name = "playRotatePics_btn";
+            this.playRotatePics_btn.Size = new System.Drawing.Size(75, 35);
+            this.playRotatePics_btn.TabIndex = 14;
+            this.playRotatePics_btn.Text = "播放";
+            this.playRotatePics_btn.UseVisualStyleBackColor = true;
+            this.playRotatePics_btn.Click += new System.EventHandler(this.playRotatePics_btn_Click);
+            // 
+            // playPics_picb
+            // 
+            this.playPics_picb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.playPics_picb.Location = new System.Drawing.Point(534, 22);
+            this.playPics_picb.Name = "playPics_picb";
+            this.playPics_picb.Size = new System.Drawing.Size(400, 400);
+            this.playPics_picb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.playPics_picb.TabIndex = 13;
+            this.playPics_picb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 1033);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.playPics_picb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button create_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox originPictureAngle_tbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox playPics_picb;
+        private System.Windows.Forms.Button playRotatePics_btn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox finalPictureAngle_tbx;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox deltaAngle_tbx;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Image originPicture;
+        float originAngle;
+        float deltaAngle;
+        float finalAngle;
+        public Form1()
+        {
+            InitializeComponent();
+            originPictureAngle_tbx.Text = "0";  // 默认当前图片的旋转角度为0°
+            deltaAngle_tbx.Text = "1";   // 默认每次增加1°;
+            finalPictureAngle_tbx.Text = "180";  // 默认最后一张图片的旋转调度为180°;
+
+            //    // 初始化图片
+            //for (int i = 1; i <= 360; i++)
+            //{
+            //    PictureBox pictureBox = new PictureBox();
+            //    pictureBox.Image = (System.Drawing.Bitmap)(Properties.Resources.ResourceManager.GetObject($"_{i}"));
+            //    pictureBox.Size = new Size(80, 80);
+            //    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            //    flowLayoutPanel1.Controls.Add(pictureBox);
+            //}
+
+        }
+        /// <summary>
+        /// 点击显示当前剪切板中的图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            originPicture = GetClipBoardImage();
+            pictureBox1.Image = originPicture;
+        }
+
+
+        #region 按指定角度生成旋转后的图片
+        private void create_btn_Click(object sender, EventArgs e)
+        {
+            float currentAngle = originAngle;
+            while (currentAngle < finalAngle)
+            {
+                currentAngle += deltaAngle;
+                Image newImage = KiRotate((Bitmap)originPicture, currentAngle, Color.Transparent);
+                pictureBox1.Image = newImage;
+                newImage.Save($"_{currentAngle}.png", System.Drawing.Imaging.ImageFormat.Png);
+                PictureBox pictureBox = new()
+                {
+                    Image = newImage,
+                    Size = new Size(80, 80),
+                    SizeMode = PictureBoxSizeMode.StretchImage
+                };
+                flowLayoutPanel1.Controls.Add(pictureBox);
+            }
+        }
+        #region 图片任意角度旋转
+        /// <summary>
+        /// 图片任意角度旋转
+        /// </summary>
+        /// <param name="bmp">原始图Bitmap</param>
+        /// <param name="angle">旋转角度</param>
+        /// <param name="bkColor">背景色</param>
+        /// <returns>输出Bitmap</returns>
+        public static Bitmap KiRotate(Bitmap bmp, float angle, Color bkColor)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+
+            PixelFormat pf;
+
+            if (bkColor == Color.Transparent)
+            {
+                pf = PixelFormat.Format32bppArgb;
+            }
+            else
+            {
+                pf = bmp.PixelFormat;
+            }
+
+            Bitmap tmp = new Bitmap(w, h, pf);
+            Graphics g = Graphics.FromImage(tmp);
+            g.Clear(bkColor);
+            g.DrawImageUnscaled(bmp, 1, 1);
+            g.Dispose();
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+            Matrix mtrx = new Matrix();
+            mtrx.Rotate(angle);
+            RectangleF rct = path.GetBounds(mtrx);
+
+            Bitmap dst = new Bitmap((int)rct.Width, (int)rct.Height, pf);
+            g = Graphics.FromImage(dst);
+            g.Clear(bkColor);
+            g.TranslateTransform(-rct.X, -rct.Y);
+            g.RotateTransform(angle);
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.DrawImageUnscaled(tmp, 0, 0);
+            g.Dispose();
+            tmp.Dispose();
+            GC.Collect();
+            return dst;
+        }
+        #endregion
+
+        #region 旋转任意角度方法二
+
+        /// <summary>
+        /// 获取原图像绕中心任意角度旋转后的图像
+        /// </summary>
+        /// <param name="rawImg"></param>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        public Image GetRotateImage(Image srcImage, int angle)
+        {
+            angle = angle % 360;
+            //原图的宽和高
+            int srcWidth = srcImage.Width;
+            int srcHeight = srcImage.Height;
+            //图像旋转之后所占区域宽和高
+            Rectangle rotateRec = GetRotateRectangle(srcWidth, srcHeight, angle);
+            int rotateWidth = rotateRec.Width;
+            int rotateHeight = rotateRec.Height;
+            //目标位图
+            Bitmap destImage = null;
+            Graphics graphics = null;
+            try
+            {
+                //定义画布，宽高为图像旋转后的宽高
+                destImage = new Bitmap(rotateWidth, rotateHeight);
+                //graphics根据destImage创建，因此其原点此时在destImage左上角
+                graphics = Graphics.FromImage(destImage);
+                //要让graphics围绕某矩形中心点旋转N度，分三步
+                //第一步，将graphics坐标原点移到矩形中心点,假设其中点坐标（x,y）
+                //第二步，graphics旋转相应的角度(沿当前原点)
+                //第三步，移回（-x,-y）
+                //获取画布中心点
+                Point centerPoint = new Point(rotateWidth / 2, rotateHeight / 2);
+                //将graphics坐标原点移到中心点
+                graphics.TranslateTransform(centerPoint.X, centerPoint.Y);
+                //graphics旋转相应的角度(绕当前原点)
+                graphics.RotateTransform(angle);
+                //恢复graphics在水平和垂直方向的平移(沿当前原点)
+                graphics.TranslateTransform(-centerPoint.X, -centerPoint.Y);
+                //此时已经完成了graphics的旋转
+
+                //计算:如果要将源图像画到画布上且中心与画布中心重合，需要的偏移量
+                Point Offset = new Point((rotateWidth - srcWidth) / 2, (rotateHeight - srcHeight) / 2);
+                //将源图片画到rect里（rotateRec的中心）
+                graphics.DrawImage(srcImage, new Rectangle(Offset.X, Offset.Y, srcWidth, srcHeight));
+                //重至绘图的所有变换
+                graphics.ResetTransform();
+                graphics.Save();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (graphics != null)
+                    graphics.Dispose();
+            }
+            return destImage;
+        }
+        /// <summary>
+        /// 计算矩形绕中心任意角度旋转后所占区域矩形宽高
+        /// </summary>
+        /// <param name="width">原矩形的宽</param>
+        /// <param name="height">原矩形高</param>
+        /// <param name="angle">顺时针旋转角度</param>
+        /// <returns></returns>
+        public Rectangle GetRotateRectangle(int width, int height, float angle)
+        {
+            double radian = angle * Math.PI / 180; ;
+            double cos = Math.Cos(radian);
+            double sin = Math.Sin(radian);
+            //只需要考虑到第四象限和第三象限的情况取大值(中间用绝对值就可以包括第一和第二象限)
+            int newWidth = (int)(Math.Max(Math.Abs(width * cos - height * sin), Math.Abs(width * cos + height * sin)));
+            int newHeight = (int)(Math.Max(Math.Abs(width * sin - height * cos), Math.Abs(width * sin + height * cos)));
+            return new Rectangle(0, 0, newWidth, newHeight);
+        }
+        #endregion
+        #endregion
+
+        #region 获得图片
+
+        /// <summary>
+        /// 获取剪切板中的图片
+        /// </summary>
+        private Image GetClipBoardImage()
+        {
+            Image image = null;
+            IDataObject iData = Clipboard.GetDataObject();
+            if (iData.GetDataPresent(DataFormats.MetafilePict))
+            {
+                var img = Clipboard.GetImage();
+                image = img;
+            }
+            else if (iData.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = Clipboard.GetFileDropList();
+                if (files.Count == 0) { return null; }
+                image = Image.FromFile(files[0]);
+            }
+
+            else if (iData.GetDataPresent(DataFormats.Text))
+            {
+                var path = (String)iData.GetData(DataFormats.Text);
+                var chars = Path.GetInvalidPathChars();
+                if (path.IndexOfAny(chars) >= 0)
+                {
+                    MessageBox.Show("路径中包含非法字符！");
+                    return null;
+                }
+                if (System.IO.File.Exists(path))
+                {
+                    var name = Path.GetFileNameWithoutExtension(path);
+                    var extension = path.Substring(path.LastIndexOf("."));
+                    string imgType = ".png|.jpg|.jpeg";
+                    if (imgType.Contains(extension.ToLower()))
+                    {
+                        image = Image.FromFile(path);
+                    }
+                    else
+                    {
+                        MessageBox.Show("格式错误！");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("文件不存在！");
+                }
+            }
+            return image;
+        }
+        #endregion
+
+
+
+        private void currentPictureAngle_tbx_TextChanged(object sender, EventArgs e)
+        {
+            originAngle = float.Parse(originPictureAngle_tbx.Text);
+        }
+
+        private void deltaAngle_TextChanged(object sender, EventArgs e)
+        {
+            deltaAngle = float.Parse(deltaAngle_tbx.Text);
+        }
+
+        private void lastPictureAngle_tbx_TextChanged(object sender, EventArgs e)
+        {
+            finalAngle = float.Parse(finalPictureAngle_tbx.Text);
+        }
+        #region 打开文件位置
+
+        private void OpenFileLocation_Click(object sender, EventArgs e)
+        {
+            var path = Environment.CurrentDirectory;
+            var info = new ProcessStartInfo(path)
+            {
+                UseShellExecute = true,
+            };
+            Process.Start(info);    // 打开指定文件夹，选中指定文件.
+        }
+        #endregion
+
+        #region 播放旋转图片
+        int index = 0;
+        private void playRotatePics_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 10;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            playPics_picb.Image = (flowLayoutPanel1.Controls[index] as PictureBox).Image;
+            if (index < flowLayoutPanel1.Controls.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                timer1.Stop();
+                index = 0;
+            }
+        }
+        #endregion
+    }
+}
